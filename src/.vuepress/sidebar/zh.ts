57a809b4a6d6a72cafae10e653c002f0d46b4085
唐皓晨 <2290774104@qq.com>
2023-02-23 11:02:41 +0800

    chore: 侧边栏调整

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,5 +1,24 @@
 import { sidebar } from 'vuepress-theme-hope';
 
 export const zhSidebar = sidebar({
-  '/前端/': ['学习笔记', 'css', '项目搭建']
+  '/前端/': [
+    {
+      text: '学习笔记',
+      collapsible: true,
+      prefix: '学习笔记/',
+      children: 'structure'
+    },
+    {
+      text: 'css',
+      collapsible: true,
+      prefix: 'css/',
+      children: 'structure'
+    },
+    {
+      text: '项目搭建',
+      collapsible: true,
+      prefix: '项目搭建/',
+      children: 'structure'
+    }
+  ]
 });
