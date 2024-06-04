63e91f93e883e16b83dbcabc3d064bdbfa83b7ff
2290774104 <2290774104@qq.com>
2024-06-04 10:01:28 +0800

    perf: element-plus 组件汉化

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,21 +1,24 @@
-import { defineClientConfig } from "@vuepress/client";
+import { defineClientConfig } from '@vuepress/client';
 
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css"
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import zhCn from 'element-plus/es/locale/lang/zh-cn.mjs';
 
-import PictureBase64 from "./components/picture-base64.vue";
-import Base64Picture from "./components/base64-picture.vue";
-import CreateUuid from './components/create-uuid.vue'
-import LetterHandle from './components/letter-handle.vue'
+import PictureBase64 from './components/picture-base64.vue';
+import Base64Picture from './components/base64-picture.vue';
+import CreateUuid from './components/create-uuid.vue';
+import LetterHandle from './components/letter-handle.vue';
+import TimeConversion from './components/time-conversion.vue';
 
 export default defineClientConfig({
   enhance({ app }) {
-    app.use(ElementPlus)
+    app.use(ElementPlus, { locale: zhCn });
 
-    app.component("PictureBase64", PictureBase64);
-    app.component("Base64Picture", Base64Picture);
-    app.component("CreateUuid", CreateUuid);
-    app.component("LetterHandle", LetterHandle);
+    app.component('PictureBase64', PictureBase64);
+    app.component('Base64Picture', Base64Picture);
+    app.component('CreateUuid', CreateUuid);
+    app.component('LetterHandle', LetterHandle);
+    app.component('TimeConversion', TimeConversion);
   },
 
   setup() {},
