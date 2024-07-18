fa0a9cdbf3f8845cf27663552776e2972fdfe07c
2290774104 <2290774104@qq.com>
2024-07-18 17:49:09 +0800

    feat: 增加qiankun集成vue3+vite文档

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -87,6 +87,7 @@ export default navbar([
         children: [
           { text: 'qiankun集成vue2+webpack', link: 'qiankun集成vue2+webpack' },
           { text: 'qiankun集成vue2+vite', link: 'qiankun集成vue2+vite' },
+          { text: 'qiankun集成vue3+vite', link: 'qiankun集成vue3+vite' },
         ],
       },
     ],
