225c66da6fbc093c176dafa5397e7f2678cbdfdd
2290774104 <2290774104@qq.com>
2024-08-09 10:09:52 +0800

    test: 增加demo模块入口文件快照测试

--- src/.vuepress/components/demo/__test__/index.spec.ts
+++ src/.vuepress/components/demo/__test__/index.spec.ts
@@ -1,11 +1,14 @@
-import { createApp } from 'vue'
-import { describe, test } from 'vitest';
+import { createApp } from 'vue';
+import { describe, expect, test } from 'vitest';
 
 import Demo from '../index';
 
 describe('demo', () => {
   test('组件使用', async () => {
-    const app = createApp({})
-    app.use(Demo)
+    const app = createApp({});
+    app.use(Demo);
+  });
+  test('快照测试', () => {
+    expect(Demo).toMatchSnapshot();
   });
 });
